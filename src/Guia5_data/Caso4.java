1c1744aaa371fa1d2e17cc14dc69113e8570cda4
51967 <51967@192.168.1.5>
2021-05-06 22:22:46 -0500

    Actualizacion 6/5

---
+++ src/Guia5_data/Caso4.java
@@ -0,0 +1,48 @@
+package Guia5_data;
+
+import java.util.ArrayList;
+
+
+import com.senati.eti.model.Publicacion;
+
+public class Caso4 {
+
+	public static void main(String[] args) {
+		
+		Publicacion objPu1 = new Publicacion("Los Perros Hambrientos","Ciro Alegria", 1);
+		Publicacion objPu2 = new Publicacion("Tradiciones Peruanas","Ricardo Palma", 1);
+		Publicacion objPu3 = new Publicacion("Informatica Global","Bill Gates", 2);
+		Publicacion objPu4 = new Publicacion("Los Heraldos Negros","Cesar Vallejo", 1);
+		Publicacion objPu5 = new Publicacion("Rev. Dig. Merca2.0","Mark Zuckerberg", 2);
+		
+		
+        ArrayList<Publicacion> arrPublicacion = new ArrayList<Publicacion>();
+		
+		arrPublicacion.add(objPu1);
+		arrPublicacion.add(objPu2);
+		arrPublicacion.add(objPu3);
+		arrPublicacion.add(objPu4);
+		arrPublicacion.add(objPu5);
+		
+		System.out.println("\n*****************");
+		System.out.println("Datos registrados");
+		System.out.println("*****************");
+		
+		String patron = "%-4s %-30s %-20s %-20s";
+		System.out.println(String.format(patron, "N", "TITULO", "AUTOR", "TIPO"));
+		System.out.println(String.format(patron, "-", "------", "------","----"));
+		
+		
+		for(int x = 0; x < arrPublicacion.size(); x++) {
+			System.out.println(String.format(patron, (x+1),
+					arrPublicacion.get(x).Titulo(),
+					arrPublicacion.get(x).Autor(),
+					arrPublicacion.get(x).MostrarTipo()));
+		}
+	
+		
+		
+
+	}
+
+}
---
+++ src/com/senati/eti/model/Publicacion.java
@@ -0,0 +1,79 @@
+package com.senati.eti.model;
+
+import java.util.ArrayList;
+
+public class Publicacion {
+
+	private String nombre;
+	private String autor;
+	private int tipo;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	public Publicacion() {
+	}
+	
+	
+	public Publicacion(String nombre, String autor, int tipo) {
+		this.nombre = nombre;
+		this.autor = autor;
+		this.tipo = tipo;
+	}
+	
+	public String Titulo() {
+		
+		return this.nombre;
+
+	}
+	
+    public String Autor() {
+		
+		return this.autor;
+
+	}
+	
+	
+	
+	
+	public String MostrarTipo() {
+		String r = "";
+		
+		if(this.tipo == 1)
+			r = "Libro";
+		else if(this.tipo == 2)
+			r = "Revista";
+		else
+			r = "Otros";
+		
+		return r;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
