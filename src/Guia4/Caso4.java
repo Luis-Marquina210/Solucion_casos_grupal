e64ecc67078a9392dea2755462cbd104326669d0
51967 <51967@192.168.1.5>
2021-05-06 19:14:31 -0500

    Caso 4 ----> guia 4

---
+++ src/Guia4/Caso4.java
@@ -0,0 +1,44 @@
+package Guia4;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Caso4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] numeros = new int [5];
+		
+		int i = 0, pares= 0, impares= 0;
+		float promedio = 0, suma = 0;
+		
+		 for( i = 0; i < 5; i++) {
+			 System.out.print("Ingrese numero " + (i+1) + ": "  );
+			 numeros[i] = sc.nextInt();
+			 
+			 if(numeros[i] % 2 ==0)
+				 pares++;
+			 else
+				 impares++;
+			 
+			 suma = suma + numeros[i];
+			 
+		 }
+		
+		 promedio = suma / 5;
+		 
+		 
+		 System.out.println("------------");
+		 System.out.println("RESUSLTADOS");
+		 System.out.println("------------");
+		System.out.println("Cantidad de números pares........: " + pares);
+		System.out.println("Cantidad de números impares......: " + impares);
+		System.out.println("Promedio de los números..........: " + promedio);
+		}
+		
+		
+		
+		
+		   }
+	
+
+
